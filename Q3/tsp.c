13eeeeeaed48172f1c51bd61ef7ebd0b62adbaea
LYJ <21600522@handong.edu>
2017-06-02 13:37:08 +0900

    Append gr17, tsp

--- Q3/tsp.c
+++ Q3/tsp.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int m[17][17] ;
 
@@ -6,17 +10,36 @@ int path[17] ;
 int used[17] ;
 int length = 0 ;
 int min = -1 ;
+pid_t tmp;
+
+void handler(int signum) {
+	int s;
+	
+	if(tmp != getpid()) {
+		kill(getpid(), SIGINT);
+		wait(&s);
+		exit(1);
+	}
+}
 
 void _travel(int idx) {
+	char buf[128];
+
+        sprintf(buf, "%d.sol", getpid());
+
+        FILE * out;
+
 	int i ;
 
 	if (idx == 17) {
 		if (min == -1 || min > length) {
+			out = fopen(buf, "w");
 			min = length ;
-			printf("%d (", length) ;
+			fprintf(out, "%d (", length) ;
 			for (i = 0 ; i < 16 ; i++) 
-				printf("%d ", path[i]) ;
-			printf("%d)\n", path[16]) ;
+				fprintf(out, "%d ", path[i]) ;
+			fprintf(out, "%d)\n", path[16]) ;
+			fclose(out);
 		}
 	}
 	else {
@@ -41,7 +64,16 @@ void travel(int start) {
 }
 
 int main() {
-	int i, j, t ;
+	int i, j, t, status, min_l = 9999;
+	pid_t pid ;
+	char buf[128];
+	int num[17];
+	char c;
+	int len;
+	
+	tmp = getpid();
+
+	signal(SIGINT, handler);
 
 	FILE * fp = fopen("gr17.tsp", "r") ;
 
@@ -53,6 +85,40 @@ int main() {
 	}
 	fclose(fp) ;
 
-	for (i = 0  ; i < 17 ; i--) 
-		travel(i) ;
+	for (i = 0 ; i < 17 ; i++) {
+		if((pid = fork()) == 0){
+			travel(i) ;
+			exit(1);
+		}
+	}
+
+	for (i = 0 ; i<17; i++) {
+		wait(&status);
+	}
+
+	i = 1;
+
+	FILE * in;
+
+	do {
+		sprintf(buf, "%d.sol", getpid()+i);
+		in = fopen(buf, "r");
+		fscanf(in, " %d", &len);
+		if(min_l > len) {
+			min_l = len;
+			fscanf(in, " %c", &c);
+			for ( j = 0; j < 17; j++) {
+				fscanf(in, " %d", &num[j]);
+			}
+		}
+		fclose(in);
+		i++;
+	}while(i <= 17);
+
+	printf("\n%d (", min_l) ;
+	for (i = 0 ; i < 16 ; i++)
+		printf("%d ", num[i]) ;
+	printf("%d)\n", num[16]) ;
+
+	exit(0);
 }
