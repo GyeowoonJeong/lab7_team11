d58c4215ac33558b6a03076b7847de8eeb8d3f85
LYJ <21600522@handong.edu>
2017-06-02 14:22:41 +0900

    Modify q2.c. Please do 'gcc q2.c csapp.c', 'ls *.gz | ./a.out'

--- Q2/q2.c
+++ Q2/q2.c
@@ -2,22 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv, char **envp) {
-	int i, status;
+int main() {
+	int i = 0, j, status;
 	FILE* pfilename;
 	pid_t pid;
 	char buf[128];
+	char input[128];
 
-	for(i=1; argv[i]; i++) {
-		if ((pid = Fork()) == 0) {
-			sprintf(buf, "gzip -d %s", argv[i]);
+	while(fgets(input, sizeof(input), stdin) != NULL ){
+		if (Fork() == 0) {
+			sprintf(buf, "gzip -d %s", input);
 			system(buf);
 			exit(1);
 		}
+		i++;
 	}
 	
-	for(i=1; argv[i]; i++) {
-		wait(&status);
+	for(j=0; j < i; j++) {
+		pid = wait(&status);
 	}
 	
 	printf("It's Done!\n");
